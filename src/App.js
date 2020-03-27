c07cb54812c9465b2f598a37d337266d47ab7f4c
Ana Selvaraj <ana@selvaraj.org>
2020-03-27 01:07:30 -0700

    Made a single-page react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,115 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Accordion from 'react-bootstrap/Accordion'
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownButton from 'react-bootstrap/DropdownButton'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookList: null
+    };
+    this.updateSorting = this.updateSorting.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('http://book-reminder-api-tancred.us-west-1.elasticbeanstalk.com/book-reminder')
+      .then(response => response.json())
+      .then(bookList => this.setState({ bookList }));
+  }
+
+  renderBookItem(index) {
+    return (
+      <BookItem key={"book" + index} book={this.state.bookList[index]} index={index} />
+    );
+  }
+
+  renderAllBookItems() {
+    if (!this.state.bookList)
+      return null;
+    else {
+      return (<div className="book-area"><Accordion defaultActiveKey="0">
+        {[...Array(this.state.bookList.length).keys()].map(index => this.renderBookItem(index))}
+      </Accordion>
+      </div>
+      );
+    }
+  }
+
+  updateSorting(type) {
+    var list = JSON.parse(JSON.stringify(this.state.bookList));
+    switch (type) {
+      case "Title":
+        list = list.sort((a, b) => (a.title > b.title) ? 1 : -1);
+        break;
+      case "Author":
+        list = list.sort((a, b) => (a.author > b.author) ? 1 : -1);
+        break;
+      case "Decreasing Priority":
+        list = list.sort((a, b) => (a.priorityLevel < b.priorityLevel) ? 1 : -1);
+        break;
+      case "Increasing Number of Pages":
+        list = list.sort((a, b) => (a.pages > b.pages) ? 1 : -1);
+        break;
+      default:
+        list = [];
+    }
+    this.setState({ bookList: list });
+  }
+
+  render() {
+    return (<div>
+      <div className="sorting-bar">
+      <SortingBar updateSorting={this.updateSorting} />
+      </div>
+      
+      {this.renderAllBookItems()}
+    </div>);
+  }
+}
+
+class SortingBar extends React.Component {
+  sortingOptions = ["Title", "Author", "Decreasing Priority", "Increasing Number of Pages"];
+
+  renderSortingOption(str) {
+    return (
+      <Dropdown.Item className="dropdownbox" onClick={() => this.props.updateSorting(str)} key={str}>{str}</Dropdown.Item>
+    );
+  }
+
+  render() {
+    return (
+      <DropdownButton className="dropdownbox" id="dropdown-basic-button" title="Sort By" variant="danger">
+        {this.sortingOptions.map((str) => this.renderSortingOption(str))}
+      </DropdownButton>
+    );
+  }
+
+}
+
+class BookItem extends React.Component {
+  render() {
+    return (
+      <div>
+        <Card className="card">
+          <Card.Header>
+            <Accordion.Toggle as={Button} variant="link" eventKey={this.props.index.toString()}>
+              {this.props.book.title}
+            </Accordion.Toggle>
+          </Card.Header>
+          <Accordion.Collapse eventKey={this.props.index.toString()}>
+            <Card.Body>Author: {this.props.book.author}
+              <br />Number of Pages: {this.props.book.pages}
+              <br />Started? {this.props.book.started.toString() ? "Yes" : "No"}
+              <br />Priority Level: {this.props.book.priorityLevel}</Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </div>
+    )
+  }
 }
 
 export default App;
